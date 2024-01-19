8626aa0a1825df423a0dad51aaf322d6b0df9c9d
pinkivic <steimberg.victor@gmail.com>
2024-01-19 21:33:31 +0100

    Recursion + README PDF
    added a pdf that explains everything!
    
    Co-Authored-By: sophieminos <87713100+sophieminos@users.noreply.github.com>
    Co-Authored-By: Shieda <95466513+YaniSadki@users.noreply.github.com>
    Co-Authored-By: BENBEN5959 <118439991+benben5959@users.noreply.github.com>

--- src/main/scala/helpers/ObjFileGenerator.scala
+++ src/main/scala/helpers/ObjFileGenerator.scala
@@ -1,36 +1,36 @@
-package helpers
-
-import scala.collection.mutable.ArrayBuffer
 import java.io.{BufferedWriter, FileWriter}
 import components.*
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 
 object ObjFileGenerator {
 
   def writeTrianglesToObj(triangles: ArrayBuffer[Triangle], filename: String): Unit = {
     val file = new BufferedWriter(new FileWriter(filename))
+
+    @tailrec
+    def writeTrianglesRec(index: Int): Unit = {
+      if (index < triangles.length) {
+        val triangle = triangles(index)
+        // vertices
+        file.write(s"v ${triangle.point1.x} ${triangle.point1.y} ${triangle.point1.z}\n")
+        file.write(s"v ${triangle.point2.x} ${triangle.point2.y} ${triangle.point2.z}\n")
+        file.write(s"v ${triangle.point3.x} ${triangle.point3.y} ${triangle.point3.z}\n")
+
+        // face
+        val vertexIndex = index * 3 + 1
+        file.write(s"f $vertexIndex ${vertexIndex + 1} ${vertexIndex + 2}\n")
+
+        writeTrianglesRec(index + 1)
+      }
+    }
+
     try {
-      var vertexIndex = 1
-      val vertexMap = scala.collection.mutable.Map[Vertice, Int]()
-
-      for (triangle <- triangles) {
-        for (vertex <- Array(triangle.point1, triangle.point2, triangle.point3)) {
-          if (!vertexMap.contains(vertex)) {
-            file.write(s"v ${vertex.x} ${vertex.y} ${vertex.z}\n")
-            vertexMap(vertex) = vertexIndex
-            vertexIndex += 1
-          }
-        }
-      }
-
-      for (triangle <- triangles) {
-        val v1Index = vertexMap(triangle.point1)
-        val v2Index = vertexMap(triangle.point2)
-        val v3Index = vertexMap(triangle.point3)
-        file.write(s"f $v1Index $v2Index $v3Index\n")
-      }
+      writeTrianglesRec(0)
     } finally {
       file.close()
     }
   }
 
 }
+
